740fd2dd8e5532724f5a9f7315dc86b24ea43862
Esa Salminen <esa_salminen@hotmail.com>
2022-06-21 11:56:07 +0300

    day 10 completed

---
+++ day10_dad-jokes/script.js
@@ -0,0 +1,32 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// *****USING AN ASYNC/AWAIT*****
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+  const response = await fetch('https://icanhazdadjoke.com', config)
+  const data = await response.json()
+  jokeEl.innerHTML = data.joke
+}
+
+// *****USING A PROMISE*****
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
